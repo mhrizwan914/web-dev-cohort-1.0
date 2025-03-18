169e57a2a54af63e9635a33c234af8bf4c6c86ad
mhrizwan914 <mhrizwan914@gmail.com>
2025-03-18 19:18:26 +0500

    Complete Challenegs

---
+++ js/test.js
@@ -0,0 +1,68 @@
+// function workingDays(days) {
+//     let num_of_days = 0
+//     let i = 0
+//     while (i < days.length) {
+//         if (days[num_of_days] !== "Saturday" && days[num_of_days] !== "Sunday") {
+//             num_of_days++
+//         }
+//         i++
+//     }
+
+//     return num_of_days
+// }
+
+// let results = workingDays(["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"])
+
+// console.log(results)
+
+
+// function totalStars(starLevels = [["*", "*", "*"], ["*", "*"], ["*"]]) {
+//     let total = 0
+//     for (let i = 0; i < starLevels.length; i++) {
+//         total += starLevels[i].length
+//     }
+//     return total
+// }
+
+// let results = totalStars()
+
+// console.log(results)
+
+// function totalPrice(prices = [1.5, 2.3, 3.7]) {
+//     let total = 0
+//     for (let i = 0; i < prices.length; i++) {
+//         total += prices[i]
+//     }
+//     return total
+// }
+
+// let results = totalPrice()
+
+// console.log(results)
+
+// function addGuest(guestList, newGuest) {
+//     // Add the newGuest to guestList and return the updated list
+//     let updated_list = guestList.push(newGuest)
+
+//     console.log(updated_list)
+// }
+
+// addGuest(["Ahmed", "Bilal"].push, "Rizwan")
+
+// function filterHealthy(foodList) {
+//     // Remove unhealthy food and return updated list
+//     let new_list = foodList.filter((food) => food !== "Burger")
+
+//     return new_list
+// }
+// let result = filterHealthy(["Salad", "Burger", "Apple"])
+
+// console.log(result)
+
+function writeLoveLetter(message, name) {
+    // Add name at the start of the message and return updated array
+    message.unshift(name)
+    return message
+}
+writeLoveLetter(["I", "Love", "You"], "Hoorain")
+
