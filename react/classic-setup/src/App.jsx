d84dcc68077dae7be01535c30853489599105845
mhrizwan914 <mhrizwan914@gmail.com>
2025-05-27 00:29:55 +0500

    learn usestate and useeffect

--- react/classic-setup/src/App.jsx
+++ react/classic-setup/src/App.jsx
@@ -1,3 +1,19 @@
+import { useEffect, useState } from "react";
+
 export default function App() {
-  return <div>Hello, I'm from Classic React</div>;
+  const [quote, set_quote] = useState("Loading...");
+
+  useEffect(() => {
+    fetch("https://api.freeapi.app/api/v1/public/quotes/quote/random")
+      .then((response) => response.json())
+      .then((data) => set_quote(data?.data?.content))
+      .catch((error) => console.log(error));
+  }, []);
+
+  return (
+    <div>
+      <div>Hello, I'm from Classic React</div>
+      <p>Today quote is "{quote}"</p>
+    </div>
+  );
 }
